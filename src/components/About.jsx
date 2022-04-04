4524321eb00306b8301a9dc1e844b38564b2a696
JohnnyVincent <tansacal@protonmail.com>
2022-04-04 18:16:01 +0500

    style: add initial styling for about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,21 +1,49 @@
 import React from "react";
 import styles from "./Split-section.module.css";
+import {SiAdobeillustrator} from 'react-icons/si';
+import {SiAdobephotoshop} from 'react-icons/si';
+import {SiJavascript} from 'react-icons/si';
+import {SiCss3} from 'react-icons/si';
+import {SiHtml5} from 'react-icons/si';
+import {SiReact} from 'react-icons/si';
+
+
+
+
 
 function About() {
   return (
-    <section className={styles.section}>
+    <section className={`${styles.section} ${styles.sectionReverse}`}>
       <div className={`${styles.splitSection} ${styles.bgPrimary}`}>
-        <h1>Welcome</h1>
-        <p>My name is David Gasanov. I design and develop websites.</p>
-        <p>Below you will find some of my work and personal information.</p>
+        <h1>A little bit about me</h1>
         <p>
-          Should you have any inquiries or questions, feel free to contact me
-          via the provided channels on the “contact” page.{" "}
+          I come from the Republic of Georgia, a beautiful country located on
+          the eastern end of the black sea.{" "}
         </p>
-        <hr className={`${styles.line}`} />
+        <p>
+          In the middle of 2021, I began teaching myself the basics of graphic
+          design and web development. I believe that learning is a constant
+          process for developers. Therefore, I strive to work productively{" "}
+        </p>
+        <p>
+          Moving forward, I aspire to become an excellent Front-end developer
+          and web designer. For this reason, I put a lot of daily effort into
+          acquiring new skills and knowledge.{" "}
+        </p>
+        <hr className={` ${styles.line} ${styles.lineRight}`} />
       </div>
 
-      <div className={`${styles.splitSection} ${styles.bgPattern}`}></div>
+      <div className={`${styles.splitSection} ${styles.bgPattern}`}>
+        <h2 className={`${styles.largeText} ${styles.centered}`}>SKILL STACK</h2>
+        <ul className={`${styles.skills}`}>
+          <li><SiHtml5 size={'36px'}/></li>
+          <li><SiCss3 size={'36px'}/></li>
+          <li><SiJavascript size={'36px'}/></li>
+          <li><SiReact size={'36px'}/></li>
+          <li><SiAdobeillustrator size={'36px'}/></li>
+          <li><SiAdobephotoshop size={'36px'}/></li>
+        </ul>
+      </div>
     </section>
   );
 }
