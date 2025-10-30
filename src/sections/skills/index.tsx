a08b8f9adf30741050f719643901790a934a73bd
davidGasanov <tansacal@protonmail.com>
2025-10-30 16:46:03 +0400

    update skills section

--- src/sections/skills/index.tsx
+++ src/sections/skills/index.tsx
@@ -1,13 +1,154 @@
+import { FaBootstrap, FaCode, FaCss3, FaHtml5, FaSass } from "react-icons/fa6";
 import Container from "../../components/container";
+import { FaDev, FaReact } from "react-icons/fa";
+import { SiMui, SiShadcnui, SiTypescript, SiVite } from "react-icons/si";
+import { RiNextjsFill, RiTailwindCssFill } from "react-icons/ri";
+import { MdStyle } from "react-icons/md";
+import type { IconType } from "react-icons";
+import SkillCard from "./skill-card";
+import { twMerge } from "tailwind-merge";
+
+export interface SkillType {
+  title: string;
+  description?: string;
+  type: "container" | "child";
+  Icon: IconType;
+  children?: SkillType[];
+  className?: string; // for stylingw
+}
+
+const skills: SkillType[] = [
+  {
+    title: "Frontend Technology",
+    type: "container",
+    className: 'lg:col-span-8',
+    Icon: FaCode,
+    children: [
+      {
+        title: "Languages, Frameworks/Libraries",
+        Icon: FaDev,
+        type: "container",
+        children: [
+          {
+            title: "HTML",
+            type: "child",
+            Icon: FaHtml5,
+          },
+          {
+            title: "CSS",
+            type: "child",
+            Icon: FaCss3,
+          },
+          {
+            title: "Typescript",
+            type: "child",
+            Icon: SiTypescript,
+          },
+          {
+            title: "React",
+            type: "child",
+            Icon: FaReact,
+          },
+          {
+            type: "child",
+            title: "Vite",
+            Icon: SiVite,
+          },
+          {
+            title: "NextJS",
+            type: "child",
+            Icon: RiNextjsFill,
+          },
+        ],
+      },
+      {
+        title: "Styling tools",
+        Icon: MdStyle,
+        type: "container",
+        children: [
+          {
+            title: "MUI",
+            type: "child",
+            Icon: SiMui,
+          },
+          {
+            title: "Shadcn",
+            Icon: SiShadcnui,
+            type: "child",
+          },
+          {
+            title: "Tailwind",
+            Icon: RiTailwindCssFill,
+            type: "child",
+          },
+          {
+            title: "Bootstrap",
+            Icon: FaBootstrap,
+            type: "child",
+          },
+          {
+            title: "Sass",
+            Icon: FaSass,
+            type: "child",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: "Styling tools",
+    className: 'lg:col-span-4',
+    Icon: MdStyle,
+    type: "container",
+    children: [
+      {
+        title: "MUI",
+        type: "child",
+        Icon: SiMui,
+      },
+      {
+        title: "Shadcn",
+        Icon: SiShadcnui,
+        type: "child",
+      },
+      {
+        title: "Tailwind",
+        Icon: RiTailwindCssFill,
+        type: "child",
+      },
+      {
+        title: "Bootstrap",
+        Icon: FaBootstrap,
+        type: "child",
+      },
+      {
+        title: "Sass",
+        Icon: FaSass,
+        type: "child",
+      },
+    ],
+  },
+];
 
 const SkillsSection = () => {
   return (
     <section className="w-full min-h-screen bg-dark-secondary/25">
       <Container className="pt-20 md:pt-24">
-        <div className="flex flex-col gap-3 items-center text-center">
+        <div
+          className={twMerge("flex flex-col gap-3 items-center text-center")}
+        >
           <h1 className="text-4xl md:h1-large font-bold font-montserrat text-primary">
             Skills
           </h1>
+          <h2 className="opacity-70 font-light max-w-[500px] xl:max-w-[640px] ">
+            Skills section description here
+          </h2>
+        </div>
+
+        <div className="mt-20 grid grid-cols-1 lg:grid-cols-12 gap-4">
+          {skills.map((skill, index) => (
+            <SkillCard key={skill.title} index={index} {...skill} />
+          ))}
         </div>
       </Container>
     </section>
---
+++ src/sections/skills/skill-card.tsx
@@ -0,0 +1,82 @@
+import type React from "react";
+import { twMerge } from "tailwind-merge";
+import { tv } from "tailwind-variants";
+import type { SkillType } from ".";
+
+const SkillCard: React.FC<SkillType & { index?: number }> = ({
+  title,
+  Icon,
+  children,
+  type,
+  index,
+  className,
+}) => {
+  const isFirst = index === 0;
+  const childrenHaveChildren = !!children?.find((child) =>
+    child.hasOwnProperty("children")
+  );
+
+  const card = tv({
+    base: "flex flex-col relative z-10 w-full h-full",
+    variants: {
+      type: {
+        container:
+          "rounded-2xl bg-dark-secondary/40 border border-light-primary/10 p-4 hover:border-light-primary/15 transition-all duration-200 ease-in-out shadow-md shadow-dark-secondary/50",
+        child: "",
+      },
+      isFirst: {
+        true: "p-4",
+        false: "",
+      },
+    },
+  });
+
+  const titleStyle = tv({
+    variants: {
+      type: {
+        container: "text-lg font-semibold font-montserrat",
+        child: "text-md font-roboto font-regular",
+      },
+    },
+  });
+
+  return (
+    <div
+      className={twMerge(
+        "relative w-full col-span-12 lg:col-span-6",
+        className
+      )}
+    >
+      {type === "container" && (
+        <div className="absolute w-full h-full bg-dark-primary rounded-2xl" />
+      )}
+
+      <div className={card({ type, isFirst })}>
+        <div
+          className={twMerge(
+            "flex items-center gap-2 text-light-secondary",
+            type === "child" &&
+              "p-1 px-2 rounded-2xl cursor-pointer hover:bg-dark-secondary/50"
+          )}
+        >
+          <Icon className="text-primary" size={18} />
+          <h3 className={titleStyle({ type })}>{title}</h3>
+        </div>
+        {children && (
+          <div
+            className={twMerge(
+              "rounded-2xl flex gap-4 p-2 pl-4",
+              !childrenHaveChildren && "mt-2 flex-col gap-2"
+            )}
+          >
+            {children?.map((child) => (
+              <SkillCard {...child} />
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default SkillCard;
